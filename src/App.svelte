849056a349a6a547c00b86b43585c1591aa26cb0
Dennis Sonntag <dennis@node-1w7jr9ydeebyvw10e6zl3nz73.ipv6.telus.net>
2024-08-22 12:39:02 -0600

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    import WorldMap from "./assets/worldMap.svelte";
+
+	let input:string;
+
+
+</script>
+
+<main class="bg-gray-500 w-scren h-screen grid place-content-center">
+
+	<div class="w-fit h-fit flex flex-col gap-4 items-center">
+
+	<WorldMap guess={ input }/>
+
+	<input bind:value={input} type="text" name=""  class="">
+
+		<p>{input}</p>
+
+	</div>
+</main>
+
